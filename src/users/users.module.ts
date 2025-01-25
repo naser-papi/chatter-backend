42da08fa6fbf4ddde9bd96acc5c402eb78dbe475
Naser Papi <naser.papi.dev@gmail.com>
2025-01-25 22:00:49 +0330

    updating new messages solution 1

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,7 +2,7 @@ import { Module } from "@nestjs/common";
 import { UsersService } from "./users.service";
 import { UsersResolver } from "./users.resolver";
 import { UsersRepository } from "./users.repository";
-import { DatabaseModule } from "../common/database/database.module";
+import { DatabaseModule } from "@/common/database/database.module";
 import { UserDocument, UserSchema } from "./entities/user.schema";
 
 @Module({
